0915dc8b9005d7228ecb6e3f04be058919e830e7
Ivan Vanderbyl <ivanvanderbyl@me.com>
2012-09-20 12:30:39 +1000

    Added more server tests

--- tests/orion_vm/models/server_tests.rb
+++ tests/orion_vm/models/server_tests.rb
@@ -1,7 +1,5 @@
 Shindo.tests('Fog::Compute::OrionVM | server', ['orion_vm']) do
 
-  # pending if Fog.mocking?
-
   service = Fog::Compute::OrionVM.new
   options = { :hostname => "test.fog_server_#{Time.now.to_i.to_s}", :memory => 1024 }
 
@@ -9,47 +7,40 @@ Shindo.tests('Fog::Compute::OrionVM | server', ['orion_vm']) do
     @instance = service.servers.new(options)
     @instance.save
     returns(false) { @instance.new_record? }
-    returns(true) { @instance.destroy }
-  end
 
-  tests('volumes') do
-    @instance = service.servers.create(options)
-
-    tests('create') do
-      volume = @instance.volumes.create(size: 50)
-      tests('volume exists').returns(true) { volume.ready? }
-      #returns(@instance.id) { volume.server_id }
-      # puts @instance.inspect
+    tests("changing ram") do
+      @instance.memory = 2048
       @instance.reload
-      returns(1) { @instance.disks.size }
-      returns(1) { @instance.volumes.size }
+      returns(2048) { @instance.memory }
     end
 
-    tests('removing a volume') do
-      tests("volume can be loaded").returns(true) { !!@instance.volumes.first }
-      tests("it removes a volume").returns(true) do
-        volume = @instance.volumes.first
-        volume.detach
-      end
-    end
-
-    @instance.destroy
-  end
-
-  service.volumes.each do |volume|
-    volume.destroy if volume.name =~ /test\.fog_server_/
+    returns(true) { @instance.destroy }
   end
 
   model_tests(service.servers, options, true) do
 
+    @instance.addresses.create
+    @instance.volumes.create(:image => 'ubuntu-oneiric', :size => 50)
+
+    tests('#start').succeeds do
+      @instance.start
+      @instance.reload
+      returns('starting') { @instance.state }
+      @instance.wait_for { ready? }
+      @instance.ready?
+    end
+
+
+    tests("#stop").succeeds do
+      @instance.stop
+      @instance.reload
+      @instance.wait_for { stopped? }
+      @instance.stopped?
+    end
+
   end
-#     # @instance.wait_for { ready? }
 #
 #
-#     # tests('#start').succeeds do
-#     #   @instance.start
-#     #   returns('starting') { @instance.state }
-#     # end
 #
 #     # tests('#reboot("SOFT")').succeeds do
 #     #   @instance.reboot('SOFT')

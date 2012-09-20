70c5daffe941be68181367489d59a0b02eba8fda
Ivan Vanderbyl <ivanvanderbyl@me.com>
2012-09-20 14:54:17 +1000

    WHITESPACE

--- lib/fog/orion_vm/requests/compute/drop_vm.rb
+++ lib/fog/orion_vm/requests/compute/drop_vm.rb
@@ -2,12 +2,12 @@ module Fog
   module Compute
     class OrionVM
       class Real
-        
+
         def drop_vm(vm_id, options = nil)
           options ||= {}
           post('drop_vm', {:vmid => vm_id}, {:response_type => :boolean}.merge(options))
         end
-        
+
       end
     end
   end

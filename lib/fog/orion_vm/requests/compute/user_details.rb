41812ba748b6c112c62f39b579b9b5b83370bd52
Ivan Vanderbyl <ivanvanderbyl@me.com>
2012-10-01 16:17:57 +1000

    Added vlan_pool request

--- lib/fog/orion_vm/requests/compute/user_details.rb
+++ lib/fog/orion_vm/requests/compute/user_details.rb
@@ -2,12 +2,12 @@ module Fog
   module Compute
     class OrionVM
       class Real
-        
+
         def user_details(options = nil)
           options ||= {}
           get('user_details', options)
         end
-        
+
       end
     end
   end

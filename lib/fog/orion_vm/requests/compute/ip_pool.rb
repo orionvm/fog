74af7ceaa4c4832f608e81e506ca5f2e28cf72bc
Ivan Vanderbyl <ivanvanderbyl@me.com>
2012-10-01 16:17:37 +1000

    Mock ip_pool

--- lib/fog/orion_vm/requests/compute/ip_pool.rb
+++ lib/fog/orion_vm/requests/compute/ip_pool.rb
@@ -20,6 +20,48 @@ module Fog
         end
 
       end
+
+      class Mock
+        def ip_pool(filters = nil)
+          response = Excon::Response.new.tap do |response|
+            response.status = 200
+            response.body = [
+              {
+                'locked' => true,
+                'ip' => "49.156.19.56",
+                'vmid' => 841,
+                'up' => 0,
+                'down' => 0,
+                'friendly' => "test.fog_server"
+              },
+              {
+                'down' => 0,
+                'ip' => "156.17.23.28",
+                'locked' => false,
+                'friendly' => "vnctest",
+                'up' => 0
+              },
+              {
+                'down' => 0,
+                'ip' => "123.234.123.234",
+                'locked' => false,
+                'friendly' => "test.address",
+                'up' => 0
+              }
+            ]
+          end
+
+          unless filters.empty? || response.body.empty?
+            filters.each do |filter, requirement|
+              if response.body.all? { |disk| disk.keys.include?(filter.to_s) }
+                response.body = response.body.select { |disk| disk[filter.to_s] == requirement }
+              end
+            end
+          end
+
+          response
+        end
+      end
     end
   end
 end

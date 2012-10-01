4e569c784bcc56e36040cdf8b6a5dd8838341a81
Ivan Vanderbyl <ivanvanderbyl@me.com>
2012-10-01 16:17:01 +1000

    Mock disk_pool

--- lib/fog/orion_vm/requests/compute/disk_pool.rb
+++ lib/fog/orion_vm/requests/compute/disk_pool.rb
@@ -2,12 +2,12 @@ module Fog
   module Compute
     class OrionVM
       class Real
-        
+
         def disk_pool(filters = nil, options = nil)
           filters ||= {}
           options ||= {}
           response = get('disk_pool', options)
-          
+
           unless filters.empty? || response.body.empty?
             filters.each do |filter, requirement|
               if response.body.all? { |disk| disk.keys.include?(filter.to_s) }
@@ -15,10 +15,31 @@ module Fog
               end
             end
           end
-          
+
           response
         end
-        
+
+      end
+
+      class Mock
+        def disk_pool(filters = nil, options = nil)
+          response = Excon::Response.new.tap do |response|
+            response.status = 200
+            response.body = [
+              {:name => "test.fog_server_1", :image => 'ubuntu-oneiric', :size => 50, :locked => false}
+            ]
+          end
+
+          unless filters.empty? || response.body.empty?
+            filters.each do |filter, requirement|
+              if response.body.all? { |disk| disk.keys.include?(filter.to_s) }
+                response.body = response.body.select { |disk| disk[filter.to_s] == requirement }
+              end
+            end
+          end
+
+          response
+        end
       end
     end
   end

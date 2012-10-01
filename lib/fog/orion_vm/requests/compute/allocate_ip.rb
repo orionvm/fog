0ba49fa45786fe3a3005b4b02fdc340b1e058e28
Ivan Vanderbyl <ivanvanderbyl@me.com>
2012-10-01 16:13:54 +1000

    Fixed allocate_ip tests

--- lib/fog/orion_vm/requests/compute/allocate_ip.rb
+++ lib/fog/orion_vm/requests/compute/allocate_ip.rb
@@ -2,7 +2,7 @@ module Fog
   module Compute
     class OrionVM
       class Real
-        
+
         # Allocates an IP Address object
         #
         # ==== Parameters
@@ -14,18 +14,30 @@ module Fog
         # * response<~Excon::Response>:
         #   * body<~Hash>
         #     * ip<~String
-        def allocate_ip(hostname)
-          body = {:friendly => hostname}
+        def allocate_ip(hostname, address = nil)
+          body = {:friendly => hostname, :address => address}
+
+          if address
+            raise ArgumentError, "Address must be a valid IPv4 address" unless
+              address =~ /\b(?:\d{1,3}\.){3}\d{1,3}\b/
+          end
+
           post('allocate_ip', body, {:response_type => :hash})
         end
       end
-      
+
       class Mock
-        
-        def allocate_ip(hostname)
+
+        def allocate_ip(hostname, address=nil)
           response = Excon::Response.new
-          
-          if hostname == 'example.com'
+
+          if address && address == '123.234.123.231'
+            response.status = 409
+            raise(Excon::Errors.status_error({:expects => 200}, response))
+          elsif address
+            response.status = 200
+            response.body = {'ip' => address}
+          elsif hostname == 'example.com' || hostname =~ /^test/
             response.status = 200
             response.body = {'ip' => '123.234.123.234'}
           else
@@ -34,9 +46,9 @@ module Fog
           end
           response
         end
-        
+
       end
-      
+
     end
   end
 end
